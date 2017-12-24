d8cbb713338ad3c7ba58425317f35905962b9a28
Onur Eker <yonureker@gmail.com>
2017-12-24 08:46:21 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc(array)
+  array.sort!
+end
 
+def sort_array_desc(array)
+  array.sort!.reverse
+end
+
+def sort_array_char_count(array)
+  #use sort instead of sort_by
+  array.sort do |x,y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |elem|
+    elem[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.collect do |item|
+    item.start_with?("a")
+  end
+end
